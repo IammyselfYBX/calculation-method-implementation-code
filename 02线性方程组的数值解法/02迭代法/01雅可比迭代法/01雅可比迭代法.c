8eabe7129830d63cac858105ec5eabdb17644bc2
tony <2252176220@qq.com>
2023-10-06 15:28:02 +0800

    01雅可比迭代法

---
+++ 02线性方程组的数值解法/02迭代法/01雅可比迭代法/01雅可比迭代法.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <math.h>
+#define MAXSIZE 50
+void input(double a[MAXSIZE][MAXSIZE], double b[MAXSIZE], long n);
+void output(double x[MAXSIZE], long n);
+
+int main(int argc, char *argv[])
+{
+    double a[MAXSIZE][MAXSIZE], b[MAXSIZE], x[MAXSIZE], oldx[MAXSIZE];
+    double epsilon, e, s;
+    long n, i, j, k, maxk;
+    printf("请输入原方程组的阶数: ");
+    scanf("%ld" , &n);
+    input(a, b, n);
+    printf("请输入迭代初始向量(中间用‘ ’隔开): ");
+    for(i =0; i <= n-1; i++){
+      scanf("%lf", &oldx[i]);
+    }
+    printf("请输入最大迭代次数\n");
+    scanf("%ld", &maxk);
+    printf("请输入误差上限: ");
+    scanf("%lf", &epsilon);
+
+    for(k=1; k <= maxk; k++){
+      for(i=0; i<= n-1; i++){
+        s = 0;
+        for(j = 0; j<=n-1; j++){
+          if(j != i){
+            s += a[i][j] * oldx[j];
+          }
+        }
+        x[i] = (b[i]-s)/a[i][i];
+      }
+      e = 0;
+      for(i = 0; i<= n-1; i++){
+        if(e < fabs(x[i] - oldx[i])){
+          e = fabs(x[i] - oldx[i]);
+        }
+      }
+      if( e< epsilon){
+        break;
+      }
+      for(i=0; i<= n-1; i++){
+        oldx[i] = x[i];
+      }
+    }
+    if( k <= maxk){
+      output(x, n);
+    }else{
+      printf("迭代次数已经超过上限\n");
+    }
+    
+    return 0;
+}
+
+
+void input(double a[MAXSIZE][MAXSIZE], double b[MAXSIZE], long n)
+{
+  long i, j;
+  printf("请输入原方程的增广矩阵\n");
+  for(i=0; i <= n-1; i++){
+    for(j=0; j<= n-1; j++){
+      scanf("%lf", &a[i][j]);
+    }
+    scanf("%lf", &b[i]);
+  }
+}
+void output(double x[MAXSIZE], long n)
+{
+  long i;
+  printf("原方程组的解为: \n");
+  for(i = 0; i <= n-1; i++){
+    printf("%lf\n", x[i]);
+  }
+}
